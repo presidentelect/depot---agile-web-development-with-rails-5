d346800a7c2048d220bb6ac2add2d11ed7607534
Michael Hansen <m.hansen@pobox.com>
2017-03-02 12:29:23 +0000

    update basic cart with counter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+ 
+    session[:counter] = session[:counter] ? session[:counter] + 1 : 1
+
+    @access_count = session[:counter]
+    
+    if @access_count > 5
+      @count_notice = "You have accessed this page #{@access_count} #{'time'.pluralize(@access_count)}."
+    end
   end
 end
