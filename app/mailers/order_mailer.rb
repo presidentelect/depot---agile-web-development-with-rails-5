ad54e4b1ee8e4968fbf103a7fad4f2a5c66ed518
Michael Hansen <m.hansen@pobox.com>
2017-05-18 11:25:40 +0000

    add order email notifications

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,26 @@
+class OrderMailer < ApplicationMailer
+  default from: 'Michael Hansen <my_email_address@example.org>'
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.received.subject
+  #
+  def received(order)
+    @order = order
+
+    mail to: order.email, subject: "Pragmatic Store Order Confirmation"
+    
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+
+    mail to: order.email, subject: 'Pragmatic Store Order Shipped'
+  end
+end
